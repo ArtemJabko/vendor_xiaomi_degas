42215382793e02cf90f21306a1b7ba70b9908b65
ArtemJabko <soloeventhvh@gmail.com>
2026-09-04 16:49:37 +0300

    degas: initial vendor tree for Xiaomi 14T (MT6897)

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),degas)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,4334567518c333a860e881960c028e338c899a9e)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,432f7f02e9fcd5ab2bca4bee06c6a64e7a6f4826)
+$(call add-radio-file-sha1-checked,radio/ccu.img,f2bbd7078f24549efb554d351ef91ee1ef289c68)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,7f49d14b4706dac6b8f26a74049a44cf67e804f3)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,50ba03491767c50e6ed3fc6f540deb3fe1a3810b)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,79c2ab00c124f34ce283471e75f8c76fa9b4483a)
+$(call add-radio-file-sha1-checked,radio/dpm.img,da1047115e1bbd1f76b142f8af627cfaf87782c5)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,78646e7a4d3510e7b2cff106275af4380b062b58)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,d182ec1aa9791bbb43f8c9fc5cd4f8ed4853a7ed)
+$(call add-radio-file-sha1-checked,radio/gz.img,ca66dd080a00c9ce5b359f61be63980d698ff91e)
+$(call add-radio-file-sha1-checked,radio/lk.img,47f57378433399c25718700f32c2ea95e9338b00)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,2733049df9d8dae20e4cf24533853c0f9367ed2d)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,fcaea97d2a5878b17bf0eb3dd8b2f7714324e6de)
+$(call add-radio-file-sha1-checked,radio/modem.img,c4d7d788bc55b6a834ac500222139c7d117dd1ba)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,73333c16aa2e42ca9c3f0b54566fc2ab7a7a2b2d)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,412e7c3bcf38dc278c36a00838b045212d31bb90)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,a565ff9d6ac4fe05ef455424bda4df33e973747b)
+$(call add-radio-file-sha1-checked,radio/scp.img,a6d3bcc1542d5965687791679d08e643451ec995)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,f15e0b981c836877e2e78f56488fa5173e8b3504)
+$(call add-radio-file-sha1-checked,radio/sspm.img,ffe92b75b8fd241bc157e1060a5f04de3849178b)
+$(call add-radio-file-sha1-checked,radio/tee.img,f1997f9e7b3c121fb6779f4005aa0da3981023c9)
+$(call add-radio-file-sha1-checked,radio/vcp.img,16939cbcc32567a282a8e6c6de466f5f3c90259e)
+
+endif
